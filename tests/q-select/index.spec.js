0cef9fbf3ea9f6b2b25f36a753928be78d13e220
Qymh <415570450@qq.com>
2019-08-02 16:17:43 +0800

    feat: unit test cache

--- tests/q-select/index.spec.js
+++ tests/q-select/index.spec.js
@@ -1,8 +1,51 @@
 import QSelect from '../../packages/@qymh/q-select/src/index.ts';
 
+function mockError() {
+  return jest.spyOn(console, 'error').mockImplementation(() => {});
+}
+
+// function env() {
+//   process.env.NODE_ENV = 'development';
+// }
+
+// function prod() {
+//   process.env.NODE_ENV = 'production';
+// }
+
 describe('options', () => {
   it('works', () => {
-    const ins = new QSelect({ data: [[]] });
+    const ins = new QSelect();
     expect(ins).toBeDefined();
   });
+
+  describe('validateData', () => {
+    describe('emptyData', () => {
+      it('undefined Data', () => {
+        const ins = new QSelect({ data: undefined });
+        expect(ins.data).toStrictEqual([[{ key: '', value: '' }]]);
+      });
+
+      it('not an array', () => {
+        const ins = new QSelect({ data: {} });
+        expect(ins.data).toStrictEqual([[{ key: '', value: '' }]]);
+      });
+
+      it('empty array', () => {
+        const ins = new QSelect({ data: [] });
+        expect(ins.data).toStrictEqual([[{ key: '', value: '' }]]);
+      });
+
+      describe('not ganged data', () => {
+        it('not contain arrays', () => {
+          const error = mockError();
+          new QSelect({ data: [[], [], 123] });
+          expect(error).toHaveBeenCalled();
+        });
+      });
+    });
+  });
+
+  // it('NotGangedData', () => {
+
+  // })
 });
