335e7c7a17f3943247018eb02113f0f577c6ed5d
Qymh <415570450@qq.com>
2020-02-06 18:01:04 +0800

    chore: 0.4.10 build packages

--- packages/@qymh/q-select/dist/q-select.esm.js
+++ packages/@qymh/q-select/dist/q-select.esm.js
@@ -1,5 +1,5 @@
 /**
- * @qymh/q-select v0.4.8
+ * @qymh/q-select v0.4.9
  * (c) 2020 Qymh
  * @license MIT
  */
@@ -1247,6 +1247,7 @@ var Layer = (function () {
             if (!trigger || resetIndex) {
                 this.dynamicIndex[y] = 0;
             }
+            this.dynamicIndex[y] = this.getDisabledAfterIndex(dataTransLater[y], this.dynamicIndex[y]);
             this.dynamicData[y] = __assign(__assign({}, dataTransLater[y][this.dynamicIndex[y]]), { index: this.dynamicIndex[y] });
             this.touchs[y].reset(dataTransLater[y], resetIndex);
         }
--- packages/@qymh/vue-q-select/dist/q-select.common.js
+++ packages/@qymh/vue-q-select/dist/q-select.common.js
@@ -1,5 +1,5 @@
 /**
- * @qymh/vue-q-select v0.4.8
+ * @qymh/vue-q-select v0.4.9
  * (c) 2020 Qymh
  * @license MIT
  */
